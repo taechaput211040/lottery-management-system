5de0825efd87e8bb12574c2edb7d618d9b8d2ff0
Tanakorn Aksorn <taechaphat>
2022-02-18 09:31:50 +0700

    update api

--- store/lottosetting/actions.js
+++ store/lottosetting/actions.js
@@ -1,9 +1,9 @@
-//รายชื่อธนาคาร//
+//Lotto Type : get//
 export async function getLottotype({ commit }) {
   return new Promise(async (resolve, reject) => {
     try {
       let response = await this.$axios.get(`/api/LottoType/get`);
-  resolve(response.data);
+      resolve(response.data);
     } catch (error) {
       reject(error);
     }
