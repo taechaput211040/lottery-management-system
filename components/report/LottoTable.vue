edd23cef7cba020e7ac001db76067eda25a801b8
pawaris l <bright.fe.realtech@gmail.com>
2022-07-11 13:52:59 +0700

    win/loss split user | agent, highlight profit/loss

--- components/report/LottoTable.vue
+++ components/report/LottoTable.vue
@@ -6,7 +6,7 @@
     <div v-if="isLoading"><loading-page></loading-page></div>
     <div v-else class="white rounded-lg">
       <div class="pa-3">
-        <h3>รายงานกำไรขาดทุน</h3>
+        <h3>รายงานกำไรขาดทุน ของ {{ currentUser }}</h3>
       </div>
       <div class="white rounded-lg">
         <v-btn
@@ -22,7 +22,7 @@
         <v-data-table
           class="elevation-1"
           :headers="headerTable"
-          :items="datarender.agents"
+          :items="datarender.current"
           :options.sync="optionsRender"
           hide-default-footer
         >
@@ -39,6 +39,9 @@
             >
               ไม่พบข้อมูล
             </v-alert>
+          </template>
+          <template #[`item.payout`]="{ item }">
+            {{ numberWithCommas(item.payout) }}
           </template>
           <template #[`item.payout`]="{ item }">
             {{ numberWithCommas(item.payout) }}
@@ -60,7 +63,7 @@
             </v-tooltip>
           </template>
           <template #[`item.winlose`]="{ item }">
-            {{ numberWithCommas(item.winlose) }}
+            <p :class="(item.winlose > 0) ? 'greenText' : 'redText'">{{ numberWithCommas(item.winlose) }}</p>
           </template>
           <template #[`item.turnover`]="{ item }">
             {{ numberWithCommas(item.turnover) }}</template
@@ -95,11 +98,90 @@
           </v-col>
         </v-row>
 
-        <div
-          v-show="
-            (datarender.members || []).length != 0 && !$route.query.memberid
-          "
+        <div class="mt-5 pa-3">
+          <h3>รายงานกำไรขาดทุน ของ agent ลูก</h3>
+        </div>
+        <v-data-table
+          class="elevation-1"
+          :headers="headerTable"
+          :items="datarender.agents"
+          :options.sync="optionsRender"
+          hide-default-footer
         >
+          <template #[`item.no`]="{ index }">
+            {{ pagination.rowsPerPage * (pagination.page - 1) + (index + 1) }}
+          </template>
+          <template v-slot:no-data>
+            <v-alert
+              :value="true"
+              border="left"
+              color="blue-grey"
+              type="error"
+              icon="mdi-warning"
+            >
+              ไม่พบข้อมูล
+            </v-alert>
+          </template>
+          <template #[`item.payout`]="{ item }">
+            {{ numberWithCommas(item.payout) }}
+          </template>
+          <template #[`item.payout`]="{ item }">
+            {{ numberWithCommas(item.payout) }}
+          </template>
+          <template #[`item.agent_name`]="{ item }">
+            <v-tooltip bottom color="primary">
+              <template v-slot:activator="{ on, attrs }">
+                <div
+                  v-bind="attrs"
+                  v-on="on"
+                  class="primary--text font-weight-bold"
+                  style="cursor: pointer"
+                  @click="handleClickUsername(item.agent_name, item)"
+                >
+                  {{ item.agent_name }}
+                </div></template
+              >
+              <span>คลิกเพื่อดูรายละเอียด</span>
+            </v-tooltip>
+          </template>
+          <template #[`item.winlose`]="{ item }">
+            <p :class="(item.winlose > 0) ? 'greenText' : 'redText'">{{ numberWithCommas(item.winlose) }}</p>
+          </template>
+          <template #[`item.turnover`]="{ item }">
+            {{ numberWithCommas(item.turnover) }}</template
+          >
+          <template #[`item.bet`]="{ item }">
+            {{ numberWithCommas(item.bet) }}
+          </template>
+        </v-data-table>
+        <v-row
+          align="baseline"
+          class="ma-3"
+          v-if="(datarender.agents || []).length != 0"
+        >
+          <v-col cols="12" sm="2" lg="2" xl="1">
+            <v-select
+              outlined
+              hide-details="auto "
+              dense
+              v-model="optionsRender.itemsPerPage"
+              :items="pageSizes"
+              label="รายการต่อหน้า"
+            ></v-select>
+          </v-col>
+          <v-col cols="12" sm="10" lg="10">
+            <v-pagination
+              v-model="optionsRender.page"
+              :total-visible="7"
+              :length="
+                Math.ceil(datarender.agents.length / optionsRender.itemsPerPage)
+              "
+            ></v-pagination>
+          </v-col>
+        </v-row>
+
+
+        <div>
           <div class="mt-5 pa-3">
             <h3>รายงานสมาชิก</h3>
           </div>
@@ -138,7 +220,7 @@
             </template>
 
             <template #[`item.winlose`]="{ item }">
-              {{ numberWithCommas(item.winlose) }}
+              <p :class="(item.winlose > 0) ? 'greenText' : 'redText'">{{ numberWithCommas(item.winlose) }}</p>
             </template>
             <template #[`item.turnover`]="{ item }">
               {{ numberWithCommas(item.turnover) }}</template
@@ -220,6 +302,7 @@ export default {
       selectgroup: undefined,
       datefilter: {},
       pageSizes: [5, 10, 15, 25],
+      currentUser: "",
       headerTable: [
         {
           text: "ลำดับ",
@@ -435,6 +518,9 @@ export default {
         let { data: response } = await this.getReportWinlose(params);
 
         this.datarender = response.data;
+        this.currentUser = response.data.current.agent_name;
+        this.datarender.current = [response.data.current];
+        console.log(this.datarender)
         this.pagination.rowsNumber = response.pagination.total;
         this.isLoading = false;
       } catch (error) {
@@ -502,4 +588,14 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.greenText{
+  color: #28A745;
+  font-weight: 900;
+}
+
+.redText{
+  color: #DC3545;
+  font-weight: 900;
+}
+</style>
